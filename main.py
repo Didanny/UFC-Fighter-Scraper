f4e825c3175a4d2f10bd8db1aef90eebc795ae8a
Didanny <37907412+Didanny@users.noreply.github.com>
2018-09-05 22:08:08 +0300

    Find all fighter hrefs
    find all the fighter hrefs in all weight classes and add them to fighter_hrefs list

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium  import webdriver
 
 driver = webdriver.Firefox()
 
+# List to store the weight classes 
 weight_classes = [
     'Flyweight',
     'Bantamweight',
@@ -17,9 +18,29 @@ weight_classes = [
     'Women_Featherweight'
 ]
 
+# List to store the fighter hrefs 
+fighter_hrefs = []
+
 for weight_class in weight_classes:
     driver.get('http://www.ufc.com/fighter/Weight_Class/' + weight_class)
-    fighter_names = driver.find_elements_by_class_name('fighter-name').clear()
-    print(fighter_names[0].href)
+    fighter_names = driver.find_elements_by_class_name('fighter-name')
+    
+    for fighter_name in fighter_names:
+        fighter_hrefs.append(fighter_name.get_attribute('href'))
+
+    steps = driver.find_elements_by_class_name('step')
+    step_hrefs = []
+
+    for step in steps:
+        step_hrefs.append(step.get_attribute('href'))
+    
+    for step_href in step_hrefs:
+        driver.get(step_href)
+        fighter_names = driver.find_elements_by_class_name('fighter-name')
+
+        for fighter_name in fighter_names:
+            fighter_hrefs.append(fighter_name.get_attribute('href'))
+
+
 
 
