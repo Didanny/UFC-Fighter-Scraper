361d79341678ace1d12b684fc5d2f20c36f61111
Didanny <37907412+Didanny@users.noreply.github.com>
2018-09-06 14:58:25 +0300

    Find basic info for each fighter

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ weight_classes = [
 # List to store the fighter hrefs 
 fighter_hrefs = []
 
+# Iterate over every weight class
 for weight_class in weight_classes:
     driver.get('http://www.ufc.com/fighter/Weight_Class/' + weight_class)
     fighter_names = driver.find_elements_by_class_name('fighter-name')
@@ -41,6 +42,41 @@ for weight_class in weight_classes:
         for fighter_name in fighter_names:
             fighter_hrefs.append(fighter_name.get_attribute('href'))
 
+# List to store the fighters
+fighters = []
+
+# Iterate over each fighter and store data
+for fighter_href in fighter_hrefs:
+    driver.get(fighter_href)
+
+    name = driver.find_element_by_xpath('//meta[@property=\'og:title\']').get_attribute('content')
+    record = driver.find_element_by_id('fighter-skill-record').get_attribute('innerText').split('-')
+    wins = record[0]
+    losses = record[1]
+    draws = record[2].split(',')[0]
+    summary = driver.find_element_by_id('fighter-skill-summary').get_attribute('innerText')
+    fighter_from = driver.find_element_by_id('fighter-from').get_attribute('innerText')
+    fighter_lives_in = driver.find_element_by_id('fighter-lives-in').get_attribute('innerText')
+    age = driver.find_element_by_id('fighter-age').get_attribute('innerText')
+    
+
+    fighter = {
+        'name': name,
+        'record': {
+            'wins': wins,
+            'losses': losses,
+            'draw': draws
+        },
+        'summary': summary,
+        'from': fighter_from,
+        'fights out of': fighter_lives_in,
+        'age': age
+    }
+        
+    fighters.append(fighter)
 
 
+
+driver.close()
+print(fighter_hrefs)
 
